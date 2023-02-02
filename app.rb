5e324ec858c0136d94223e54e5fe0e364ae192db
Florian <florian.sasso@outlook.fr>
2023-02-02 09:27:44 +0100

    Library set

---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/game'
+require_relative 'lib/player'
+require_relative 'lib/show'
+require_relative 'lib/boardcase'
+require_relative 'lib/board'
