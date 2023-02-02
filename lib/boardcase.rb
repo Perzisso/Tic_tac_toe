ae99d6af467dc96f0d17c46cd6002c82cd1fe6c6
Florian <florian.sasso@outlook.fr>
2023-02-02 22:02:44 +0100

    On retravaille

--- lib/boardcase.rb
+++ lib/boardcase.rb
@@ -1,9 +1,10 @@
 class BoardCases
 # Classe avec 2 attr_accessor, valeur en string (X ou 0, ou vide), ainsi que sont identifiant de case
-attr_accessor :value, :case_number
+attr_accessor :case_value, :case_id
     # Doit régler sa valeur, ainsi que son numéro de case
-    def initialize(case_number)
-        @value = ""
-        @case_number = case_number
+    def initialize(case_value, case_id)
+      @case_value = case_value
+      @case_id = case_id
+
     end
 end
--- lib/show.rb
+++ lib/show.rb
@@ -1,11 +1,15 @@
+require_relative 'lib/board'
+require_relative 'lib/boardcase'
+
 class Show
     # Afficher dans le terminal l'objet de la class Board
 
   def show_board
-    puts "#{@board[0..2].join(" | ")}"
-    puts "--|---|--"
-    puts "#{@board[3..5].join(" | ")}"
-    puts "--|---|--"
-    puts "#{@board[6..8].join(" | ")}"
+    puts "  #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts ' ---+---+---'
+    puts "  #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts ' ---+---+--- '
+    puts "  #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 end
+
